0e2fadb464d5b8fce5fac4f60daafa362040779d
sreeraganil <sreeraganil2003@gmail.com>
2025-08-22 16:19:17 +0530

    robots.txt

--- core/views.py
+++ core/views.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
 
 def robots_txt(request):
-    content = """
-    User-agent: *
-    Disallow: /admin/
-    Disallow: /upload/
-    Allow: /
-
-    Sitemap: https://gem4k.onrender.com/sitemap.xml
-    """
+    content = """User-agent: *
+Disallow: /admin/
+Disallow: /upload/
+Allow: /
+Sitemap: https://gem4k.onrender.com/sitemap.xml"""
     return HttpResponse(content, content_type="text/plain")
