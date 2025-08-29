e474563fc8b1cd5810ed76fe24c900582477a91f
sreeraganil <sreeraganil2003@gmail.com>
2025-08-29 11:42:53 +0530

    updates

--- wallpapers/admin.py
+++ wallpapers/admin.py
@@ -4,6 +4,6 @@ from .models import Wallpaper
 @admin.register(Wallpaper)
 class WallpaperAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "resolution_label", "downloads", "updated_at", "created_at")
-    search_fields = ("title","category","resolution_label")
-    list_filter = ("category","resolution_label","created_at", "updated_at")
+    search_fields = ("title","category", "device","resolution_label")
+    list_filter = ("category","resolution_label","created_at", "updated_at", "device")
     prepopulated_fields = {"slug": ("title",)}
